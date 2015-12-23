ad08dbafcf304bfce6ae31666b33afda91a85103
latinman <erivas78@gmail.com>
2015-12-23 17:20:53 -0500

    Fix the category new action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
-  before_action :set_category, except: [:index, :create]
+  before_action :set_category, except: [:index, :new, :create]
 
   def index
     @categories = Category.all
