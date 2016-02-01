3d72229a3bc1f5340f5edfb32a8d4991a02a8f53
latinman <erivas78@gmail.com>
2016-01-31 23:17:32 -0500

    Added aws-sdk gem & config to production.rb for Amazon s3 storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,11 +77,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.paperclip_defaults = {
-  #     :storage => :s3,
-  #     :s3_host_name => "tinito.s3-website-us-east-1.amazonaws.com",
-  #     :bucket => "tinito"
-  # }
+  # Sets Paperclip to Upload to Amazon S3 by default
+  config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+          :bucket => ENV['S3_BUCKET_NAME'],
+          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+  }
 
   config.action_mailer.delivery_method = :smtp
 
