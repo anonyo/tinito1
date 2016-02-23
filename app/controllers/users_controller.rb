e5b025c0be1a4e32202a4d4543e58d76a488c07b
latinman <erivas78@gmail.com>
2016-02-22 21:46:11 -0500

    Modified sign up form with cc fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :last_name, :email, :password, :password_confirmation, :card_number, :card_code, :card_month)
   end
 
   def set_user
