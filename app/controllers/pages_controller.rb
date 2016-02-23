751a790d0cbe642835db5dd89457184fe8593cf9
latinman <erivas78@gmail.com>
2016-02-23 10:14:39 -0500

    Put plan back in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   def home
     redirect_to products_path if logged_in?
-    # @plan = Plan.find(1)
+    @plan = Plan.find(1)
   end
 
   def how_it_works
